a1667e7067542f22ad91b178269bd4f7f65a87b5
mohammedahw <mohammedalihw@gmail.com>
2023-03-05 17:18:11 +0300

    change: prisma configration

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,3 +1,7 @@
 import { PrismaClient } from "@prisma/client";
+import { NODE_ENV } from "../config/env";
 
-export const prisma = new PrismaClient();
+export const prisma = new PrismaClient({
+  log: NODE_ENV === "development" ? ["query", "error", "info", "warn"] : [],
+  errorFormat: "pretty",
+});
