153ac4306728b8323454ad43f0aa34a77a20225b
mohammedahw <mohammedalihw@gmail.com>
2023-04-15 23:34:11 +0300

    fix: :bug: env config

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,11 @@
 import "express-async-errors";
 import express from "express";
 import { router } from "./routers";
-import * as dotenv from "dotenv";
 import morgan from "morgan";
 import { PORT } from "./config/env";
 import { errorHandler } from "./middlewares/error-handler";
 
 const app = express();
-dotenv.config();
 
 // middlewares
 app.use(express.json());
--- src/config/env.ts
+++ src/config/env.ts
@@ -1,3 +1,6 @@
+import * as dotenv from "dotenv";
+dotenv.config();
+
 export const PORT = process.env.PORT || (3000 as number);
 export const SECRET_KEY = process.env.SECRET_KEY as string;
 export const NODE_ENV = process.env.NODE_ENV as string;
